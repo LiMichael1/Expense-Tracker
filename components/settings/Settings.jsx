a36f74b04edf43f603912ed86085d0664d820f8a
zumbuul <melje1delje@gmail.com>
2022-06-25 14:12:58 +0200

    saving rep

--- components/settings/Settings.jsx
+++ components/settings/Settings.jsx
@@ -3,6 +3,7 @@ import Navbar from "../global/Navbar";
 import SideBar from "../global/SideBar/SideBar";
 import { GlobalPageContainer } from "../styled/global/globalStyles";
 import { MainContentContainer } from "../styled/global/globalStyles";
+import SettingsMain from "./SettingsMain";
 
 const Settings = () => {
   return (
@@ -11,6 +12,7 @@ const Settings = () => {
         <SideBar></SideBar>
         <MainContentContainer>
           <Navbar></Navbar>
+          <SettingsMain></SettingsMain>
         </MainContentContainer>
       </GlobalPageContainer>
     </>
---
+++ components/settings/SettingsMainForm.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+
+import { Formik, Form } from "formik";
+import { SettingsMainInlineWrapper } from "../styled/settings/settings";
+import FormInputFieldWithIcon from "../global/FormInputFieldWithIcon";
+import FormInputField from "../global/FormInputField";
+
+const SettingsMainForm = () => {
+  return (
+    <>
+      <Formik
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          password: "",
+          confirmPassword: "",
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(values, null, 2);
+            setSubmitting(false);
+          }, 500);
+        }}
+      >
+        <Form>
+          <SettingsMainInlineWrapper>
+            <div style={{ width: "100%" }}>
+              <FormInputField
+                label="First Name"
+                name="firstName"
+                type="text"
+                placeholder="Enter your first name"
+              ></FormInputField>
+            </div>
+            <div style={{ width: "100%" }}>
+              <FormInputField
+                label="Last Name"
+                name="lastName"
+                type="text"
+                placeholder="Enter your last name"
+              ></FormInputField>
+            </div>
+          </SettingsMainInlineWrapper>
+          <SettingsMainInlineWrapper>
+            <div style={{ width: "100%" }}>
+              <FormInputFieldWithIcon
+                label="Email"
+                name="email"
+                type="text"
+                image="email icon"
+                placeholder="Enter your email"
+              ></FormInputFieldWithIcon>
+            </div>
+          </SettingsMainInlineWrapper>
+          <SettingsMainInlineWrapper>
+            <div style={{ width: "100%" }}>
+              <FormInputFieldWithIcon
+                label="New Password"
+                name="password"
+                type="password"
+                image="lock icon"
+                placeholder="Enter your password"
+              ></FormInputFieldWithIcon>
+            </div>
+            <div style={{ width: "100%" }}>
+              <FormInputFieldWithIcon
+                label="Confirm Password"
+                name="confirmPassword"
+                type="password"
+                image="lock icon"
+                placeholder="Confirm your password"
+              ></FormInputFieldWithIcon>
+            </div>
+          </SettingsMainInlineWrapper>
+        </Form>
+      </Formik>
+    </>
+  );
+};
+
+export default SettingsMainForm;
